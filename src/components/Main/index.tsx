12a4e614b9fc307cc583ccee7dbfd2ba6b4a2975
paulo-fs <jr.paulo87@gmail.com>
2022-11-20 13:34:56 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+export default function Main() {
+	return (
+		<S.Wrapper>
+			<S.Logo
+				src=""
+				alt="Imagem de um átomo e React Avançado escrito ao lado."
+			/>
+			<S.Title>React Avançado</S.Title>
+			<S.Description>Typescripty, ReactJS e Styled Components</S.Description>
+			<S.Illustration
+				src=""
+				alt="Um desenvolvedor de frente a uma tela com código"
+			/>
+		</S.Wrapper>
+	)
+}
